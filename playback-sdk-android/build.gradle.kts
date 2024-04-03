5bc204134abdb4d47cf19ad4596bbc3dcd5870ed
Artem Yelizarov <artem.yelizarov@eleks.com>
2024-04-03 11:39:06 +0300

    [SE-810] Bump java version to 17

--- playback-sdk-android/build.gradle.kts
+++ playback-sdk-android/build.gradle.kts
@@ -25,7 +25,7 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 
     defaultConfig {
@@ -45,8 +45,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 tasks.dokkaGfm {
