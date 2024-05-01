aa67b052ca5c6e5c2c493c25d211672422725e05
Stefano Russello <stefano.russello@gmail.com>
2024-05-01 15:19:14 +0200

    Downgrading Java version only to 11 to avoid "invalid source release: 17" error

--- playback-sdk-android/build.gradle.kts
+++ playback-sdk-android/build.gradle.kts
@@ -76,8 +76,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     libraryVariants.all {
         val variant = this
