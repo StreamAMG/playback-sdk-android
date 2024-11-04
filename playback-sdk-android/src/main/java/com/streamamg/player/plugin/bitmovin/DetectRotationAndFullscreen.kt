297daff9341fd96438030791726156d2edd1757d
Oleksandr Kharchenko <oleksandr.kharchenko@eleks.com>
2024-11-04 09:11:35 +0200

    [CORE-4969] Improved detector for device rotation

--- playback-sdk-android/src/main/java/com/streamamg/player/plugin/bitmovin/DetectRotationAndFullscreen.kt
+++ playback-sdk-android/src/main/java/com/streamamg/player/plugin/bitmovin/DetectRotationAndFullscreen.kt
@@ -9,12 +9,15 @@ import android.util.Log
 import androidx.compose.runtime.*
 import androidx.compose.ui.platform.LocalContext
 import com.bitmovin.player.PlayerView
+import kotlin.math.abs
 import kotlin.math.absoluteValue
 
 @Composable
 fun DetectRotationAndFullscreen(playerView: PlayerView?, callback: (isFullscreen: Boolean) -> Unit) {
     val context = LocalContext.current
     var isLandscape by remember { mutableStateOf(false) }
+    var lastOrientation by remember { mutableStateOf(false) }
+    val threshold = 5.0f
 
     DisposableEffect(Unit) {
         val sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
@@ -26,35 +29,30 @@ fun DetectRotationAndFullscreen(playerView: PlayerView?, callback: (isFullscreen
                     val x = event.values[0]
                     val y = event.values[1]
 
-                    // Determine if the device is in landscape or portrait orientation
-                    if (x.absoluteValue > y.absoluteValue) {
-                        // Landscape mode
-                        if (!isLandscape) {
-                            isLandscape = true
-                            Log.d("Orientation", "Landscape mode detected")
-//                            playerView?.enterFullscreen()
-                            callback.invoke(true)
-                        }
-                    } else {
-                        // Portrait mode
-                        if (isLandscape) {
-                            isLandscape = false
-                            Log.d("Orientation", "Portrait mode detected")
-//                            playerView?.exitFullscreen()
-                            callback.invoke(false)
+                    val currentOrientation = x.absoluteValue > y.absoluteValue
+                    if (abs(x) > threshold || abs(y) > threshold) {
+                        if (currentOrientation != lastOrientation) {
+                            if (currentOrientation && !isLandscape) {
+                                isLandscape = true
+                                Log.d("Orientation", "Landscape mode detected")
+                                callback.invoke(true)
+                            } else if (!currentOrientation && isLandscape) {
+                                isLandscape = false
+                                Log.d("Orientation", "Portrait mode detected")
+                                callback.invoke(false)
+                            }
                         }
                     }
+                    lastOrientation = currentOrientation
                 }
             }
 
             override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
         }
 
-        // Register the sensor listener
         sensorManager.registerListener(sensorEventListener, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
 
         onDispose {
-            // Unregister the sensor listener to avoid memory leaks
             sensorManager.unregisterListener(sensorEventListener)
         }
     }
