279350e5f8ec75ce42abdd23901905caefd90e67
stefano.russello@streamamg.com <stefano.russello@streamamg.com>
2024-12-12 12:00:44 +0100

    Added stoplight configuration

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI2Njk1Mg/spectral.js?branch_slug=main&token=daa0a8c7-b4d5-49ea-af66-5ffdf5ee3581";
+export default { extends: ruleset };
